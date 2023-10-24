a8d4347d1c883ab261563cb185e5accad1c42bee
David Graber <david.graber@bluewin.ch>
2023-10-24 12:23:53 +0200

    added exclusion ef and emb

--- Dataset.py
+++ Dataset.py
@@ -5,18 +5,35 @@ from torch_geometric.data import Dataset
 
 
 class IG_Dataset(Dataset):
-    def __init__(self, root):
+    def __init__(self, root, embedding=False, edge_features=False):
         super().__init__(root)
 
         self.data_dir = root
+        self.embedding = embedding
 
         self.filepaths = [os.path.join(self.data_dir, file) for file in os.listdir(self.data_dir)]
         self.input_data = {ind:torch.load(filepath) for ind, filepath in enumerate(self.filepaths)}
 
-        # Transform labels to log space
+        # Process the input graphs
+        min = 2
+        max = 30
         for ind in self.input_data:
             grph = self.input_data[ind]
-            grph.affinity = torch.log(grph.affinity + 1)
+
+            # Transform labels to negative log space, clip and min-max scale
+            log = torch.clamp(-torch.log(grph.affinity), min, max)
+            grph.affinity = (log - min) / (max - min)
+            
+            if self.embedding:
+                grph.x = torch.cat((grph.x_lig, grph.x_prot_emb), axis=0)
+            else:
+                grph.x = torch.cat((grph.x_lig, grph.x_prot_aa), axis=0)
+
+            if not edge_features:
+                grph.edge_attr = grph.edge_attr[:,3].view(-1,1)
+                grph.edge_attr_lig = grph.edge_attr_lig[:,3].view(-1,1)
+                grph.edge_attr_prot = grph.edge_attr_prot[:,3].view(-1,1)
+
             self.input_data[ind] = grph
 
 
