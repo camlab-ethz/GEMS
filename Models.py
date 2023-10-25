21d73818e8b9e6241e10346a18bf5f596a325ccb
David Graber <david.graber@bluewin.ch>
2023-10-25 08:34:17 +0200

    added second arch

--- Models.py
+++ Models.py
@@ -40,40 +40,41 @@ class GAT0mp(torch.nn.Module):
         return x
 
 
-# '''
-# # MODEL ARCHITECTURE LARGE BASELINE WITH GLOBAL MEAN POOL 
-# '''
+'''
+# MODEL ARCHITECTURE SMALL BASELINE WITH GLOBAL MEAN POOL AND MULTIPLE ATTENTION HEADS
+'''
+class GAT1mp(torch.nn.Module):
+    def __init__(self, dropout_prob, in_channels, edge_dim):
+        super(GAT1mp, self).__init__()
 
-# class GATbaseline1_mp(torch.nn.Module):
-#     def __init__(self, dropout_prob, in_channels, edge_dim):
-#         super(GATbaseline1_mp, self).__init__()
-#         #Convolutional Layers
-#         self.conv1 = GATv2Conv(in_channels, 640, edge_dim=edge_dim)
-#         self.conv2 = GATv2Conv(640, 256, edge_dim=edge_dim)
-#         self.conv3 = GATv2Conv(256, 128, edge_dim=edge_dim)
-#         self.dropout_layer = torch.nn.Dropout(dropout_prob)
-#         self.fc1 = torch.nn.Linear(128, 64)
-#         self.bn1 = torch.nn.BatchNorm1d(64)
-#         self.fc2 = torch.nn.Linear(64, 1)
+        #Convolutional Layers
+        self.conv1 = GATv2Conv(in_channels, 512, edge_dim=edge_dim, heads=2)
+        self.conv2 = GATv2Conv(512, 1024, edge_dim=edge_dim, heads=2)
+        self.conv3 = GATv2Conv(1024, 256, edge_dim=edge_dim, heads=2)
+        self.dropout_layer = torch.nn.Dropout(dropout_prob)
+        self.fc1 = torch.nn.Linear(256, 64)
+        self.bn1 = torch.nn.BatchNorm1d(64)
+        self.fc2 = torch.nn.Linear(64, 1)
 
-#     def forward(self, graphbatch):
-#         # First convolution only between ligand and protein nodes
-#         x = self.conv1(graphbatch.x, graphbatch.edge_index, graphbatch.edge_attr)
-#         x = F.relu(x)
-#         x = self.conv2(x, graphbatch.edge_index, graphbatch.edge_attr)
-#         x = F.relu(x)
-#         x = self.conv3(x, graphbatch.edge_index, graphbatch.edge_attr)
-#         x = F.relu(x)
-#         # Pool the nodes of each interaction graph
-#         x = global_mean_pool(x, batch=graphbatch.batch)
-#         x = self.dropout_layer(x)
+    def forward(self, graphbatch):
+        
+        x = self.conv1(graphbatch.x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+        x = self.conv2(x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+        x = self.conv3(x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
 
-#         # Fully-Connected Layers
-#         x = self.fc1(x)
-#         x = self.bn1(x)
-#         x = F.relu(x)
-#         x = self.fc2(x)
-#         return x
+        # Pool the nodes of each interaction graph
+        x = global_mean_pool(x, batch=graphbatch.batch)
+        x = self.dropout_layer(x)
+
+        # Fully-Connected Layers
+        x = self.fc1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
 
 
 
