3ef0b2c7db3a1bb4d22472eab8f27db06476dc6c
David Graber (HPC) <grbv@zhaw.ch>
2023-10-31 13:03:17 +0100

    added concat models

--- Models.py
+++ Models.py
@@ -146,3 +146,110 @@ class GAT3mp(torch.nn.Module):
 
 
 
+'''
+# MODEL ARCHITECTURE SMALL WITH TWO ATTENTION HEADS (WITH CONCAT) IN THE FIRST LAYER AND GLOBAL MEAN POOL 
+'''
+class GAT1ccmp(torch.nn.Module):
+    def __init__(self, dropout_prob, in_channels, edge_dim):
+        super(GAT1ccmp, self).__init__()
+
+        #Convolutional Layers
+        self.conv1 = GATv2Conv(in_channels, 256, edge_dim=edge_dim, heads=2)
+        self.conv2 = GATv2Conv(512, 1024, edge_dim=edge_dim)
+        self.conv3 = GATv2Conv(1024, 256, edge_dim=edge_dim)
+        self.dropout_layer = torch.nn.Dropout(dropout_prob)
+        self.fc1 = torch.nn.Linear(256, 64)
+        self.fc2 = torch.nn.Linear(64, 1)
+
+    def forward(self, graphbatch):
+        
+        x = self.conv1(graphbatch.x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+        x = self.conv2(x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+        x = self.conv3(x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+
+        # Pool the nodes of each interaction graph
+        x = global_mean_pool(x, batch=graphbatch.batch)
+        x = self.dropout_layer(x)
+
+        # Fully-Connected Layers
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
+    
+
+
+'''
+# MODEL ARCHITECTURE SMALL WITH TWO ATTENTION HEADS (WITH CONCAT) IN THE FIRST TWO LAYERS AND GLOBAL MEAN POOL 
+'''
+class GAT2ccmp(torch.nn.Module):
+    def __init__(self, dropout_prob, in_channels, edge_dim):
+        super(GAT2ccmp, self).__init__()
+
+        #Convolutional Layers
+        self.conv1 = GATv2Conv(in_channels, 256, edge_dim=edge_dim, heads=2)
+        self.conv2 = GATv2Conv(512, 512, edge_dim=edge_dim, heads=2)
+        self.conv3 = GATv2Conv(1024, 256, edge_dim=edge_dim)
+        self.dropout_layer = torch.nn.Dropout(dropout_prob)
+        self.fc1 = torch.nn.Linear(256, 64)
+        self.fc2 = torch.nn.Linear(64, 1)
+
+    def forward(self, graphbatch):
+        
+        x = self.conv1(graphbatch.x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+        x = self.conv2(x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+        x = self.conv3(x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+
+        # Pool the nodes of each interaction graph
+        x = global_mean_pool(x, batch=graphbatch.batch)
+        x = self.dropout_layer(x)
+
+        # Fully-Connected Layers
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
+    
+
+
+
+
+'''
+# MODEL ARCHITECTURE SMALL WITH TWO ATTENTION HEADS (WITH CONCAT) IN ALL THREE LAYERS AND GLOBAL MEAN POOL 
+'''
+class GAT3ccmp(torch.nn.Module):
+    def __init__(self, dropout_prob, in_channels, edge_dim):
+        super(GAT3ccmp, self).__init__()
+
+        #Convolutional Layers
+        self.conv1 = GATv2Conv(in_channels, 256, edge_dim=edge_dim, heads=2)
+        self.conv2 = GATv2Conv(512, 512, edge_dim=edge_dim, heads=2)
+        self.conv3 = GATv2Conv(1024, 128, edge_dim=edge_dim, heads=2)
+        self.dropout_layer = torch.nn.Dropout(dropout_prob)
+        self.fc1 = torch.nn.Linear(256, 64)
+        self.fc2 = torch.nn.Linear(64, 1)
+
+    def forward(self, graphbatch):
+        
+        x = self.conv1(graphbatch.x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+        x = self.conv2(x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+        x = self.conv3(x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+
+        # Pool the nodes of each interaction graph
+        x = global_mean_pool(x, batch=graphbatch.batch)
+        x = self.dropout_layer(x)
+
+        # Fully-Connected Layers
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
