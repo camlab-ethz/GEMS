41acbb247b6b2eb3bd32c2ae10679001f10a52fa
David Graber <david.graber@bluewin.ch>
2023-11-30 15:47:01 +0100

    implemented smaller objects for tr

--- Dataset_DTI4.py
+++ Dataset_DTI4.py
@@ -1,66 +1,101 @@
 import os
 import numpy as np
 import torch
-from torch_geometric.data import Dataset
+from torch_geometric.data import Dataset, Data
 
 
 class IG_Dataset(Dataset):
-    def __init__(self, root, embedding=False, edge_features=False, atom_features=False, masternode=None):
+    def __init__(self, root, embedding=False, edge_features=False, atom_features=False, masternode=None, mn_self_loops=True):
         super().__init__(root)
 
         self.data_dir = root
-        self.embedding = embedding
-
         self.filepaths = [os.path.join(self.data_dir, file) for file in os.listdir(self.data_dir)]
-        
         self.input_data = {}
         
-        # Process the input graphs
+        # Process all the graphs according to kwargs
         ind = 0
-        for file in self.filepaths:
+        for file in self.filepaths[:20]:
             
             grph = torch.load(file)
+
 
             # Transform labels to negative log space and min-max scale
             min = 0
             max = 16
             pK = -torch.log10(grph.affinity)
             pK_scaled = (pK - min) / (max - min)
-            grph.y = pK_scaled
-            
-            if self.embedding:
-                grph.x = torch.cat((grph.x_lig, grph.x_prot_emb), axis=0)
-            else:
-                grph.x = torch.cat((grph.x_lig, grph.x_prot_aa), axis=0)
 
-            if not edge_features:
-                grph.edge_attr = grph.edge_attr[:,:3].view(-1,1)
-                grph.edge_attr_lig = grph.edge_attr_lig[:,:3].view(-1,1)
-                grph.edge_attr_prot = grph.edge_attr_prot[:,:3].view(-1,1)
+
+            # Generate feature matrix x with/without embedding and atom features
+            if embedding:
+                x = torch.cat((grph.x_lig, grph.x_prot_emb), axis=0)
+            else:
+                x = torch.cat((grph.x_lig, grph.x_prot_aa), axis=0)
 
             if not atom_features:
-                grph.x[:, -31:] = 0
+                x[:,-31:] = 0
 
-            grph.n_nodes = grph.x.shape[0]
-            grph.data = grph.data.reshape(1, 3)
+            # If edge_features should be excluded, inclode only edge type and length
+            if not edge_features:
+                grph.edge_attr = grph.edge_attr[:,:4]
+                #edge_attr_lig = grph.edge_attr_lig[:,:4]
+                #edge_attr_prot = grph.edge_attr_prot[:,:4]
 
+
+            # Number of nodes of the graph (including masternode)
+            n_nodes = torch.tensor(x.shape[0], dtype=torch.int64)
+
+
+            # Edge Index: If masternode, connect masternode to all atoms or only to ligand/protein atoms
             
-            # 
-            if masternode == 'all':
+            if masternode == None:
+                
+                train_graph = Data(x = x, 
+                               edge_index=grph.edge_index, 
+                               edge_attr=grph.edge_attr, 
+                               y=pK_scaled
+                               #,pos=grph.pos
+                               )
+
+            else:
+
+                # If the masternode should be connected to all other nodes
+                if masternode == 'all':
+                    edge_index_master_prot_lig = torch.cat([grph.edge_index_master_prot, grph.edge_index_master_lig], axis = 1)
+                    edge_index = torch.cat([grph.edge_index, edge_index_master_prot_lig], axis = 1)
+                    if mn_self_loops:
+                        mn_index = torch.max(edge_index).item()
+                        edge_index = torch.cat((edge_index, torch.tensor([[mn_index],[mn_index]])), axis=1)
+                        
+
+
+                # If the masternode should only be connected to ligand nodes
+                elif masternode == 'ligand':
+                    edge_index = torch.cat([grph.edge_index, grph.edge_index_master_lig], axis = 1)
+                    if mn_self_loops:
+                        mn_index = torch.max(edge_index).item()
+                        edge_index = torch.cat((edge_index, torch.tensor([[mn_index],[mn_index]])), axis=1)
+
+                # If the masternode should only be connection to protein nodes
+                elif masternode == 'protein':
+                    edge_index = torch.cat([grph.edge_index, grph.edge_index_master_prot], axis = 1)
+                    if mn_self_loops:
+                        mn_index = torch.max(edge_index).item()
+                        edge_index = torch.cat((edge_index, torch.tensor([[mn_index],[mn_index]])), axis=1)
+
+                else: 
+                    raise Exception("masternode must be either 'None', 'protein', 'ligand' or 'all'")
             
-
-            if masternode == 'ligand'
-
-            if masternode == 'protein'
-
-            # Merge the edge_index_master_lig and the edge_index_master_prot into a overall master node edge_index
-            grph.edge_index_master_prot_lig = torch.cat([grph.edge_index_master_prot, grph.edge_index_master_lig], axis = 1)
-
-            # Create an edge_index with all possible edges, convalent, non-covalent and directed master node connections
-            grph.edge_index_master = torch.cat([grph.edge_index, grph.edge_index_master_prot_lig], axis = 1)
             
-            
-            self.input_data[ind] = grph
+                train_graph = Data(x = x, 
+                                edge_index=edge_index, 
+                                edge_attr=grph.edge_attr, 
+                                y=pK_scaled, 
+                                n_nodes=n_nodes #needed for reading out masternode features
+                                #,pos=grph.pos
+                )         
+
+            self.input_data[ind] = train_graph
             ind += 1
 
 
@@ -70,28 +105,3 @@ class IG_Dataset(Dataset):
     def get(self, idx):
         graph = self.input_data[idx]
         return graph
-
-
-#     graph = Data(
-        
-            x_lig = torch.tensor(x_lig, dtype=torch.float64),
-            x_prot_emb = torch.tensor(x_prot_emb, dtype=torch.float64),
-            x_prot_aa = torch.tensor(x_prot_aa, dtype=torch.float64),
-                 
-            edge_index = edge_index,
-            edge_index_lig = edge_index_lig,
-            edge_index_prot = edge_index_prot,
-
-            edge_index_master_lig = edge_index_master_lig,
-            edge_index_master_prot = edge_index_master_prot,
-
-            edge_attr = edge_attr,
-            edge_attr_lig = edge_attr_lig,
-            edge_attr_prot = edge_attr_prot,
-
-            pos = torch.tensor(pos, dtype=torch.float64),
-            affinity= torch.tensor(affinity, dtype=torch.float64),
-
-            id = complex_id,
-            data = torch.tensor(metadata, dtype=torch.float64)
-            )
