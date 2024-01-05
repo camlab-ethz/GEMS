63ee79d4bda65a87675142b103c66514f0c46cd3
David Graber <david.graber@bluewin.ch>
2024-01-05 13:25:42 +0100

    changed architectures for DTI5

--- Models_DTI5.py
+++ Models_DTI5.py
@@ -6,6 +6,8 @@ from torch_geometric.data import Batch
     
 
 #-------------------------------------------------------------------------------------------------------------
+'''BASELINE ARCHITECTURE'''
+
 class GAT0(torch.nn.Module):
     def __init__(self, dropout_prob, in_channels, edge_dim, conv_dropout_prob):
         super(GAT0, self).__init__()
@@ -66,8 +68,8 @@ class GAT0_mn(torch.nn.Module):
         return x
 #-------------------------------------------------------------------------------------------------------------
 
+'''SECOND LAYER LARGER - The idea of this architecture is that not all input features are consensed to 256 in the second layer'''
 
-'''Just slightly more parameters'''
 class GAT1(torch.nn.Module):
     def __init__(self, dropout_prob, in_channels, edge_dim, conv_dropout_prob):
         super(GAT1, self).__init__()
@@ -77,7 +79,7 @@ class GAT1(torch.nn.Module):
         self.conv2 = GATv2Conv(1024, 128, edge_dim=edge_dim, heads=4, dropout=conv_dropout_prob)
         
         self.dropout_layer = torch.nn.Dropout(dropout_prob)
-        self.fc1 = torch.nn.Linear(1024, 64)
+        self.fc1 = torch.nn.Linear(512, 64)
         self.fc2 = torch.nn.Linear(64, 1)
 
     def forward(self, graphbatch):
@@ -97,8 +99,6 @@ class GAT1(torch.nn.Module):
         return x
 
 
-
-'''Just slightly more parameters'''
 class GAT1_mn(torch.nn.Module):
     def __init__(self, dropout_prob, in_channels, edge_dim, conv_dropout_prob):
         super(GAT1_mn, self).__init__()
@@ -108,7 +108,7 @@ class GAT1_mn(torch.nn.Module):
         self.conv2 = GATv2Conv(1024, 128, edge_dim=edge_dim, heads=4, dropout=conv_dropout_prob)
         
         self.dropout_layer = torch.nn.Dropout(dropout_prob)
-        self.fc1 = torch.nn.Linear(1024, 64)
+        self.fc1 = torch.nn.Linear(512, 64)
         self.fc2 = torch.nn.Linear(64, 1)
 
     def forward(self, graphbatch):
@@ -130,11 +130,75 @@ class GAT1_mn(torch.nn.Module):
 
 
 
-'''Much more parameters ------------------------------------------------------------------------------------------------------'''
+#-------------------------------------------------------------------------------------------------------------
 
-class GAT0b(torch.nn.Module):
+'''FIRST LAYER LARGER - The idea of this architecture is that not all input features are consensed to 256 in the FIRST layer'''
+
+class GAT2(torch.nn.Module):
     def __init__(self, dropout_prob, in_channels, edge_dim, conv_dropout_prob):
-        super(GAT0b, self).__init__()
+        super(GAT2, self).__init__()
+
+        #Convolutional Layers
+        self.conv1 = GATv2Conv(in_channels, 512, edge_dim=edge_dim, heads=4, dropout=conv_dropout_prob)
+        self.conv2 = GATv2Conv(2048, 64, edge_dim=edge_dim, heads=4, dropout=conv_dropout_prob)
+        
+        self.dropout_layer = torch.nn.Dropout(dropout_prob)
+        self.fc1 = torch.nn.Linear(256, 64)
+        self.fc2 = torch.nn.Linear(64, 1)
+
+    def forward(self, graphbatch):
+        x = self.conv1(graphbatch.x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+        x = self.conv2(x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+
+        # Pool the nodes of each interaction graph
+        x = global_add_pool(x, batch=graphbatch.batch)
+        x = self.dropout_layer(x)
+
+        # Fully-Connected Layers
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
+
+
+class GAT2_mn(torch.nn.Module):
+    def __init__(self, dropout_prob, in_channels, edge_dim, conv_dropout_prob):
+        super(GAT2_mn, self).__init__()
+
+        #Convolutional Layers
+        self.conv1 = GATv2Conv(in_channels, 512, edge_dim=edge_dim, heads=4, dropout=conv_dropout_prob)
+        self.conv2 = GATv2Conv(2048, 64, edge_dim=edge_dim, heads=4, dropout=conv_dropout_prob)
+        
+        self.dropout_layer = torch.nn.Dropout(dropout_prob)
+        self.fc1 = torch.nn.Linear(256, 64)
+        self.fc2 = torch.nn.Linear(64, 1)
+
+    def forward(self, graphbatch):
+        x = self.conv1(graphbatch.x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+        x = self.conv2(x, graphbatch.edge_index, graphbatch.edge_attr)
+        x = F.relu(x)
+
+        # Pool the nodes of each interaction graph
+        last_node_indeces = graphbatch.n_nodes.cumsum(dim=0) - 1
+        master_node_features = x[last_node_indeces]
+        x = self.dropout_layer(master_node_features)
+
+        # Fully-Connected Layers
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
+    
+
+''' ------------------------------------------------------------------------------------------------------
+FIRST LAYER AND SECOND LAYER LARGER '''
+
+class GAT3(torch.nn.Module):
+    def __init__(self, dropout_prob, in_channels, edge_dim, conv_dropout_prob):
+        super(GAT3, self).__init__()
 
         #Convolutional Layers
         self.conv1 = GATv2Conv(in_channels, 512, edge_dim=edge_dim, heads=4, dropout=conv_dropout_prob)
@@ -162,9 +226,9 @@ class GAT0b(torch.nn.Module):
         return x
 
 
-class GAT0b_mn(torch.nn.Module):
+class GAT3_mn(torch.nn.Module):
     def __init__(self, dropout_prob, in_channels, edge_dim, conv_dropout_prob):
-        super(GAT0b_mn, self).__init__()
+        super(GAT3_mn, self).__init__()
 
         #Convolutional Layers
         self.conv1 = GATv2Conv(in_channels, 512, edge_dim=edge_dim, heads=4, dropout=conv_dropout_prob)
@@ -192,46 +256,12 @@ class GAT0b_mn(torch.nn.Module):
         return x
 
 
-class GAT0b_mnp(torch.nn.Module):
-    def __init__(self, dropout_prob, in_channels, edge_dim, conv_dropout_prob):
-        super(GAT0b_mnp, self).__init__()
-
-        #Convolutional Layers
-        self.conv1 = GATv2Conv(in_channels, 512, edge_dim=edge_dim, heads=4, dropout=conv_dropout_prob)
-        self.conv2 = GATv2Conv(2048, 128, edge_dim=edge_dim, heads=4, dropout=conv_dropout_prob)
-        
-        self.dropout_layer = torch.nn.Dropout(dropout_prob)
-        self.fc1 = torch.nn.Linear(1024, 64)
-        self.fc2 = torch.nn.Linear(64, 1)
-
-    def forward(self, graphbatch):
-        x = self.conv1(graphbatch.x, graphbatch.edge_index, graphbatch.edge_attr)
-        x = F.relu(x)
-        x = self.conv2(x, graphbatch.edge_index, graphbatch.edge_attr)
-        x = F.relu(x)
-
-        # Pool the nodes of each interaction graph
-        last_node_indeces = graphbatch.n_nodes.cumsum(dim=0) - 1
-        master_node_features = x[last_node_indeces]
-
-        master_node_features = torch.cat((master_node_features, global_add_pool(x, batch=graphbatch.batch)), dim=1)
-
-        x = self.dropout_layer(master_node_features)
-        # Fully-Connected Layers
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        return x
-
-
-
-
 
 
 
 
 '''3-LAYER ARCHITECTURE ------------------------------------------------------------------------------------------------------'''
-class GAT0c_mn(torch.nn.Module):
+class GAT4_mn(torch.nn.Module):
     def __init__(self, dropout_prob, in_channels, edge_dim, conv_dropout_prob):
         super(GAT0c_mn, self).__init__()
 
@@ -256,8 +286,6 @@ class GAT0c_mn(torch.nn.Module):
         # Pool the nodes of each interaction graph
         last_node_indeces = graphbatch.n_nodes.cumsum(dim=0) - 1
         master_node_features = x[last_node_indeces]
-
-        master_node_features = torch.cat((master_node_features, global_add_pool(x, batch=graphbatch.batch)), dim=1)
 
 
         x = self.dropout_layer(master_node_features)
