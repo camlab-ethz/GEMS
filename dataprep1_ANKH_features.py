c0696c1f4fd5c6a304c810c5d69c1dd76fddf31c
David Graber <dagraber@sam.math.ethz.ch>
2024-05-10 16:40:47 +0200

    new script to compute ANKH features

---
+++ dataprep1_ANKH_features.py
@@ -0,0 +1,123 @@
+import pickle
+import os
+import ankh
+import torch
+import numpy as np
+import argparse
+from tqdm import tqdm
+from Bio.PDB.PDBParser import PDBParser
+from f_parse_pdb_general import parse_pdb
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser(description='Preprocess PDBbind data for DTI5')
+    parser.add_argument('--data_dir', type=str, required=True, help='Path to the data directory containing all proteins(PDB) and ligands (SDF)')
+    parser.add_argument('--ankh_base', default=False, type=lambda x: x.lower() in ['true', '1', 'yes'], help="If the ankh_base model should be used")
+    return parser.parse_args()
+
+args = arg_parser()
+
+data_dir = args.data_dir
+model_name = 'ankh_base' if args.ankh_base else 'ankh_large'
+
+# Initialize PDB Parser
+parser = PDBParser(PERMISSIVE=1, QUIET=True)
+
+
+# Device settings and loading of models
+device = torch.device(f'cuda:0' if torch.cuda.is_available() else 'cpu')
+print(torch.cuda.is_available())
+print(device)
+
+if model_name == 'ankh_base':
+    model, tokenizer = ankh.load_base_model()
+    embedding_size = 768
+elif model_name == 'ankh_large':
+    model, tokenizer = ankh.load_large_model()
+    embedding_size = 1536
+
+model.to(device).eval()
+
+
+
+
+# Initialize Log File
+log_file_path = os.path.join(data_dir, '.logs', f'{model_name}.txt')
+log = open(log_file_path, 'a')
+log.write("Generating ANKH Embeddings - Log File:\n")
+log.write("\n")
+
+# Generate a lists of all proteins
+proteins = sorted([protein for protein in os.scandir(data_dir) if protein.name.endswith('protein.pdb')], key=lambda x: x.name)
+num_proteins = len(proteins)
+
+print(f'Number of Proteins to be processed: {num_proteins}')
+print(f'Model Name: {model_name}')
+
+
+# FUNCTION TO COMPUTE EMBEDDINGS
+
+def get_aa_embeddings_ankh(protein_sequence):
+
+    protein_sequences = [list(protein_sequence)]
+    outputs = tokenizer.batch_encode_plus(protein_sequences, 
+                                add_special_tokens=False, 
+                                padding=True, 
+                                is_split_into_words=True, 
+                                return_tensors="pt")
+    outputs.to(device)
+
+    with torch.no_grad():
+        embedding = model(input_ids=outputs['input_ids'], attention_mask=outputs['attention_mask']).last_hidden_state
+
+    return embedding.squeeze() 
+
+
+
+for protein in tqdm(proteins):
+
+    id = protein.name.split('_')[0]
+    log_string = f'{id}: '
+
+    save_filepath = os.path.join(data_dir, f'{id}_{model_name}.pt')
+    if os.path.exists(save_filepath):
+        log_string += 'Embedding already exists'
+        log.write(log_string + "\n")
+        continue
+
+    expected_len = 0
+
+    with open(protein.path) as pdbfile:
+        prot = parse_pdb(parser, id, pdbfile)    
+        
+    emb = np.array([], dtype=np.int64).reshape(0,embedding_size)
+
+    for chain in prot:
+
+        chain_comp = prot[chain]['composition']
+        if chain_comp == [True, False] or chain_comp == [True, True]:
+            
+            sequence = prot[chain]['aa_seq']
+            expected_len += len(sequence)
+
+            try:
+                embeddings = get_aa_embeddings_ankh(sequence)
+                emb = np.vstack((emb, embeddings.cpu()))
+                
+            except Exception as e:
+                log_string += str(e)
+                break
+
+
+
+    if not emb.shape == (expected_len, embedding_size):
+        log_string += f'Embedding has wrong shape {emb.shape} instead of ({expected_len}x{embedding_size})'
+        log.write(log_string + "\n")
+        continue
+    else:
+        torch.save(emb, save_filepath)
+        log_string += 'Successful'
+
+    log.write(log_string + "\n")
+
+log.close()
