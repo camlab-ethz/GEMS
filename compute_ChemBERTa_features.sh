61deea2234394fb2f57eeba34bc9792d65dffc7f
David Graber <dagraber@sam.math.ethz.ch>
2024-06-10 18:17:29 +0200

    sh scripts for embeddings

---
+++ compute_ChemBERTa_features.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=ankh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=01:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --gpus=1
+#SBATCH --tmp=20G
+#SBATCH --gres=gpumem:20G
+
+module load eth_proxy
+
+source /cluster/project/math/dagraber/miniconda3/etc/profile.d/conda.sh
+conda activate graphgen
+
+
+python dataprep1_ChemBERTa_features.py --data_dir /cluster/work/math/dagraber/DTI/PDBbind1 --model ChemBERTa-10M-MLM
