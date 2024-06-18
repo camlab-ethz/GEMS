73e1e8495654fecd2bedc8341ed12444a9ee6352
David Graber <dagraber@sam.math.ethz.ch>
2024-06-18 11:24:29 +0200

    work in progress

--- PDBbind_training.sh
+++ PDBbind_training.sh
@@ -18,40 +18,114 @@ source /cluster/project/math/dagraber/miniconda3/etc/profile.d/conda.sh
 conda activate PYG
 
 
-# Define variables for the parameters
+#---------------------------------
+# General Parameters
+#---------------------------------
+dataset="c4"
+working_dir="/cluster/work/math/dagraber/DTI/"
 project_name="DTI5"
-filtering="c4"
+experiment="DTI5f_${dataset}"
+run_name="testrun_06"
+
+#---------------------------------
+# Dataset Construction
+#---------------------------------
+data_dir="/cluster/work/math/dagraber/DTI/PDBbind/"
+
+data_split="PDBbind_data_splits/PDBbind_${dataset}_data_split.json"
+experiment_path="${working_dir}/experiments/${experiment}/${run_name}/"
+
+refined_only="False"
+exclude_ic50="False"
+exclude_nmr="False"
+resolution_threshold=5
+precision_strict="False"
+
+protein_embeddings=("ankh_base" "ankh_large")
+ligand_embeddings=("ChemBERTa_10M")
+masternode="True"
+masternode_connectivity="all"
+masternode_edges="undirected"
+atom_features="True"
+edge_features="True"
+delete_ligand="False"
+delete_protein="False"
+
+train_dataset_path="PDBbind_${dataset}_train_dataset.pt"
+if [ ! -f "$train_dataset_path" ]; then
+    echo "Constructing Training Dataset"
+    python PDBbind_construct_dataset.py \
+        --data_dir $data_dir \
+        --data_split $data_split \
+        --dataset train \
+        --save_path $train_dataset_path \
+        --refined_only $refined_only --exclude_ic50 $exclude_ic50 --exclude_nmr $exclude_nmr \
+        --resolution_threshold $resolution_threshold --precision_strict $precision_strict \
+        --protein_embeddings $protein_embeddings --ligand_embeddings $ligand_embeddings \
+        --masternode $masternode --masternode_connectivity $masternode_connectivity --masternode_edges $masternode_edges \
+        --atom_features $atom_features --edge_features $edge_features \
+        --delete_ligand $delete_ligand --delete_protein $delete_protein
+    echo "Construction of Training Dataset complete"
+else
+    echo "Training dataset already exists at $train_dataset_path. Skipping dataset construction."
+fi
+
+
+casf2013_dataset_path="PDBbind_casf2013_dataset.pt"
+if [ ! -f "$casf2013_dataset_path" ]; then
+    echo "Constructing CASF2013 Dataset"
+    python PDBbind_construct_dataset.py \
+        --data_dir $data_dir \
+        --data_split $data_split \
+        --dataset casf2013 \
+        --save_path $casf2013_dataset_path \
+        --refined_only $refined_only --exclude_ic50 $exclude_ic50 --exclude_nmr $exclude_nmr \
+        --resolution_threshold $resolution_threshold --precision_strict $precision_strict \
+        --protein_embeddings $protein_embeddings --ligand_embeddings $ligand_embeddings \
+        --masternode $masternode --masternode_connectivity $masternode_connectivity --masternode_edges $masternode_edges \
+        --atom_features $atom_features --edge_features $edge_features \
+        --delete_ligand $delete_ligand --delete_protein $delete_protein
+    echo "Construction of CASF2013 Dataset complete"
+else
+    echo "CASF2013 dataset already exists at $casf2013_dataset_path. Skipping dataset construction."
+fi
+
+
+casf2016_dataset_path="PDBbind_casf2016_dataset.pt"
+if [ ! -f "$casf2016_dataset_path" ]; then
+    echo "Constructing CASF2016 Dataset"
+    python PDBbind_construct_dataset.py \
+        --data_dir $data_dir \
+        --data_split $data_split \
+        --dataset casf2016 \
+        --save_path $casf2016_dataset_path \
+        --refined_only $refined_only --exclude_ic50 $exclude_ic50 --exclude_nmr $exclude_nmr \
+        --resolution_threshold $resolution_threshold --precision_strict $precision_strict \
+        --protein_embeddings $protein_embeddings --ligand_embeddings $ligand_embeddings \
+        --masternode $masternode --masternode_connectivity $masternode_connectivity --masternode_edges $masternode_edges \
+        --atom_features $atom_features --edge_features $edge_features \
+        --delete_ligand $delete_ligand --delete_protein $delete_protein
+    echo "Construction of CASF2016 Dataset complete"
+else
+    echo "CASF2016 dataset already exists at $casf2016_dataset_path. Skipping dataset construction."
+fi
+
+
+#---------------------------------
+# Training
+#---------------------------------
+
+# Define variables for the parameters
+
 wandb="True"
-
-#############################
-
-experiment="DTI5f_c4"
-run_name="00_testrun_all_in_one"
 
 conv_dropout=0
 dropout=0
 
 num_epochs=10
-fold_to_train=(0 1 2 3 4)
+fold_to_train=(0)
 
-masternode="all"
 model="GAT4mnbn"
-
-# Dataset Composition
-emb="False"
-embedding_descriptor="ankh_base"
-ef="True"
-af="True"
-refined_only="False"
-exclude_ic50="False"
-exclude_nmr="False"
-resolution_threshold=3.0
-precision_strict="False"
-
-# If either protein nodes or ligand nodes should be omitted (not both)
-# Should only be used if masternode = "all"
-delete_protein="False"
-delete_ligand="False"
 
 
 # Training Hyperparameters
@@ -83,20 +157,6 @@ min_lr=0.0001
 
 #############################
 
-working_dir="/cluster/work/math/dagraber/DTI"
-test_data_dir="/cluster/work/math/dagraber/DTI/test_data/"
-
-# Path to data - Unpack the data into temporary directory in local scratch
-archive_file="${project_name}${filtering}_train_data_${embedding_descriptor}.tar.gz"
-DATA_ARCHIVE="${working_dir}/training_data/${project_name}${filtering}/${archive_file}"
-cp $DATA_ARCHIVE $TMPDIR
-echo "Training Data ${archive_file} moved to ${TMPDIR}"
-cd $TMPDIR
-tar -xzf $archive_file
-echo "Training Data unpacked"
-
-# Path to the unpacked data
-data_dir="${TMPDIR}/training_data/"
 
 
 # Create a folder in the working directory for the run
@@ -109,6 +169,7 @@ copy_results() {
     unset WANDB_API_KEY
     }
 trap copy_results EXIT
+
 
 
 all_stdicts=()
@@ -138,14 +199,24 @@ for fold in "${fold_to_train[@]}"; do
     train_stdout="${results_path}train-${SLURM_JOB_ID}_f${fold}.out"
     train_stderr="${results_path}train-${SLURM_JOB_ID}_f${fold}.err"
     config_train="python train.py \
-                --data_dir $data_dir --log_path $results_path --model $model --project_name $project_name --wandb $wandb --run_name $run_name \
-                --embedding $emb --edge_features $ef --atom_features $af --masternode $masternode --refined_only $refined_only --exclude_ic50 $exclude_ic50 \
-                --resolution_threshold $resolution_threshold --precision_strict $precision_strict --exclude_nmr $exclude_nmr --loss_func $loss_func --optim $optimizer \
-                --num_epochs $num_epochs --batch_size $bs --learning_rate $lr --weight_decay $wd --dropout $dropout --conv_dropout $conv_dropout \
+                --dataset_path $train_dataset_path \
+                --log_path $results_path \
+                --model $model \
+                --project_name $project_name \
+                --wandb $wandb \
+                --run_name $run_name \
+                --loss_func $loss_func \
+                --optim $optimizer \
+                --num_epochs $num_epochs \
+                --batch_size $bs \
+                --learning_rate $lr \
+                --weight_decay $wd \
+                --dropout $dropout \
+                --conv_dropout $conv_dropout \
                 --alr_lin $alr_lin --start_factor $start_factor --end_factor $end_factor --total_iters $total_iters \
-                --alr_mult $alr_mult --factor $factor --delete_ligand $delete_ligand --delete_protein $delete_protein \
-                --early_stopping $early_stopping --early_stop_patience $early_stop_patience --early_stop_min_delta $early_stop_min_delta \
+                --alr_mult $alr_mult --factor $factor \
                 --alr_plateau $alr_plateau --reduction $reduction --patience $patience --min_lr $min_lr \
+                --early_stopping $early_stopping --early_stop_patience $early_stop_patience --early_stop_min_delta $early_stop_min_delta \
                 --fold_to_train $fold"
 
     cd $working_dir
@@ -189,6 +260,7 @@ for fold in "${fold_to_train[@]}"; do
     # ---------------------------------------------------------------------------------
 
 done
+
 
 # Aggregate the results of all trained folds into a csv file
 echo "Aggregating Results"
