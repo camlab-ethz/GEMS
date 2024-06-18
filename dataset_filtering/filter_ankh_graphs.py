38ccd983ba82538a6a5f4fa702c5d41817014f1e
David Graber <dagraber@sam.math.ethz.ch>
2024-06-18 18:01:30 +0200

    moved or updated

---
+++ dataset_filtering/filter_ankh_graphs.py
@@ -0,0 +1,15 @@
+import os
+
+template_path = '/data/grbv/PDBbind/DTI_5c4b/input_graphs_ankh_base/training_data'
+destination_path = '/data/grbv/PDBbind/DTI_5c4b/input_graphs_ankh_base_unpad/training_data'
+
+
+template_ids = [filename[0:4] for filename in os.listdir(template_path) if filename.endswith('.pt')]
+#print(template_ids)
+
+# Iterate over the destination path and delete a file if it is not in the template_ids list
+
+for filename in os.listdir(destination_path):
+    if filename.endswith('.pt') and filename[0:4] not in template_ids:
+        os.remove(os.path.join(destination_path, filename))
+        print(f"Removed {filename}")
