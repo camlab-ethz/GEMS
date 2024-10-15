b5feaa5cdd53c5c72fa497c6a7f6e86a1600930e
David Graber <dagraber@sam.math.ethz.ch>
2024-10-15 11:48:33 +0200

    changed env

--- inference.sh
+++ inference.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=8G
-#SBATCH --time=00:10:00
+#SBATCH --time=00:20:00
 #SBATCH --gres=gpu:1
 #SBATCH --gpus=1
 #SBATCH --tmp=20G
@@ -13,7 +13,6 @@
 module load eth_proxy
 
 source /cluster/project/math/dagraber/miniconda3/etc/profile.d/conda.sh
-conda activate PYG
+conda activate BAP
 
-
-python inference.py --model_arch GATE18d --dataset_path dataset_casf2016.pt 
+python inference.py --model_arch GATE18d --dataset_path inference_test/dataset_inference_151024.pt
