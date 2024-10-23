b7ae5091fc0d4763a3620701ae61d19041646820
David Graber <dagraber@sam.math.ethz.ch>
2024-10-23 15:57:36 +0200

    renamed folder

---
+++ search_algorithms/data_leakage_ligands_c9.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=leakage
+#SBATCH --output=data_leakage_ligands_c9.log
+#SBATCH --time=02:00:00
+#SBATCH --nodes=1
+#SBATCH --mem-per-cpu=16G
+
+module load eth_proxy
+
+source /cluster/project/math/dagraber/miniconda3/etc/profile.d/conda.sh
+conda activate graphgen
+
+python data_leakage_ligands.py --data_split c9
